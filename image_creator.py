291b95b7c292e9a5514ef2247ef9d7b3d95a24e6
Eitan Bloch <eitan.a.bloch@gmail.com>
2022-03-09 16:40:00 +0200

    added option for binary image

--- image_creator.py
+++ image_creator.py
@@ -27,3 +27,27 @@ def create_img(x_img, y_img):
         cv2.imwrite('images/overlay.png', final)
     return final
 
+
+def create_binary_img(x_img, y_img):
+    cube = cubes[randint(0, len(cubes) - 1)]
+    new_cube = np.zeros(background.shape)
+    x_offset = int(x_img - (cube.shape[0] / 2))
+    y_offset = int(y_img - (cube.shape[1] / 2))
+
+    new_cube[y_offset:y_offset + cube.shape[0], x_offset:x_offset + cube.shape[1]] = cube
+
+    # create overlay img
+    w, h, _ = background.shape
+    final = np.zeros((w, h, 1),)
+    w, h, c = background.shape
+    for iw in range(w):
+        for ih in range(h):
+            if not new_cube[iw][ih].any():
+                final[iw][ih] = 0
+            else:
+                final[iw][ih] = 255
+
+    if write_image_to_file:
+        cv2.imwrite('images/overlay.png', final)
+    return final
+
